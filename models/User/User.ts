49f0480ee0872fcecde967b99a22ddc8cff98336
John Kim Querobines <querobinesj@gmail.com>
2022-04-28 13:48:45 +0800

    feat: User model

---
+++ models/User/User.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+  },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+
+export default UserModel;
